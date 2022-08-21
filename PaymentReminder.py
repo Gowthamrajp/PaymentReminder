c8bf7e581093b476a2956224162425c4d046ae54
Gowtham Raj <gowthrj@amazon.com>
2022-08-21 17:49:35 +0530

    made changes to message text

--- PaymentReminder.py
+++ PaymentReminder.py
@@ -17,7 +17,7 @@ def sender(number,name,amount,cycle,isPaid):
     if(isPaid!="paid"):        
         count=count+1
         unpaid+=amount
-        pywhatkit.sendwhatmsg_instantly("+"+str(number),"Dear Customer, Please pay Cable TV amount for "+cycle+" month Rs"+str(amount)+"via Gpay/Paytm/phonepay/WhatsApp\n\n  9444047656 ( Pandian )  Rs "+str(amount)+" \n  Please Send screen shot of payment receipt if possible\n   ignore if Paid \n Thank you Have a Nice day",15,True,3)
+        pywhatkit.sendwhatmsg_instantly("+"+str(number),"Dear Customer,\nPlease pay Cable TV amount for "+cycle+" month Rs"+str(amount)+"via Gpay/Paytm/phonepay/WhatsApp\n\n9444047656 ( Pandian )  Rs "+str(amount)+" \nPlease Send screen shot of payment receipt if possible\n\nIgnore if Paid \n\nThank you for your kind Co-operation.Have a Nice day"+"\U0001F64F",15,True,3)
         print(str(x+1)+". Reminder sent to "+name )
     else:
         paid+=amount
